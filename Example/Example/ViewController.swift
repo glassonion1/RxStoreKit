fe99d99a1506af8a8e1dc237d912cc6af1db898f
François Boulais <fboulais@me.com>
2017-12-16 10:12:40 +0100

    Add SKReceiptRefreshRequest support by creating associated DelegateProxy.

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxStoreKit
+import StoreKit
 
 class ViewController: UIViewController {
 
---
+++ SKReceiptRefreshRequest+Rx.swift
@@ -0,0 +1,34 @@
+//
+//  SKReceiptRefreshRequest+Rx.swift
+//  Pods-Example
+//
+//  Created by François Boulais on 16/12/2017.
+//
+
+import Foundation
+
+import StoreKit
+#if !RX_NO_MODULE
+    import RxSwift
+    import RxCocoa
+#endif
+
+extension SKReceiptRefreshRequest {
+    
+    public func createRxDelegateProxy() -> SKReceiptRefreshRequestDelegateProxy {
+        return SKReceiptRefreshRequestDelegateProxy(parentObject: self)
+    }
+    
+}
+
+extension Reactive where Base: SKReceiptRefreshRequest {
+    
+    public var delegate: DelegateProxy<SKReceiptRefreshRequest, SKRequestDelegate> {
+        return SKReceiptRefreshRequestDelegateProxy.proxy(for: base)
+    }
+    
+    public var request: Completable {
+        return SKReceiptRefreshRequestDelegateProxy.proxy(for: base).responseSubject.ignoreElements()
+    }
+    
+}
